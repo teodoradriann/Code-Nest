b534195e3e493c9ea5ebd5dc02af88386b29a187
Teodor Adrian Miron <teodormiron75@gmail.com>
2025-03-14 12:28:55 +0200

    Initial Commit

---
+++ Code Nest/Code_NestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_NestApp.swift
+//  Code Nest
+//
+//  Created by Teodor Adrian on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct Code_NestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code NestTests/Code_NestTests.swift
@@ -0,0 +1,17 @@
+//
+//  Code_NestTests.swift
+//  Code NestTests
+//
+//  Created by Teodor Adrian on 3/14/25.
+//
+
+import Testing
+@testable import Code_Nest
+
+struct Code_NestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
